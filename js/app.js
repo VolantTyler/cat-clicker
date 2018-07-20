99d5f7f519630604a749398acd1b36dbd923f41b
VolantTyler <33227593+VolantTyler@users.noreply.github.com>
2018-07-20 15:33:12 -0400

    feat: Buttons display

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-// let clickCounter = 0;
-// let countField = $('#count');
-$(function() {
 
-    let model = [
+//$(function() {
+
+    let model = {
+        currentCat: null;
+        cats: [
         {
             name: 'Cat1',
             clickCount: 0,
@@ -28,27 +29,52 @@ $(function() {
             clickCount: 0,
             image: 'images/cat_picture5.jpg'
         }
-        ];
+        ]};
 
 
     let octopus = {
 
         init: function() {
             viewButtons.init();
+        },
+
+        catsArray: function() {
+            return model;
+        },
+
+        setCurrentCat: function(cat) {
+            model.currentCat = cat;
         }
     };
 
 
     let viewButtons = {
         init: function() {
-            const buttonArea = $('#catButtons');
-            //remove model from view to octopus
-            for(let i=0; i<model.length; i++) {
-                let cat = model[i];
-                buttonArea.appendChild('<button id="'+cat.name+'">'+cat.name+'</button>');
-            }
+            this.buttonArea = document.getElementById('catButtons');
 
-           // this.render();
+            this.render();
+        },
+
+        render: function() {
+            let i, cat, entry;
+            this.buttonArea.innerHTML = '';
+            let cats = octopus.catsArray();
+
+            //remove model from view to octopus
+            for(i=0; i<cats.length; i++) {
+                cat = cats[i];
+                entry = document.createElement('button');
+                entry.textContent = cat.name;
+
+                entry.addEventListener('click', (function(catCopy) {
+                    return function() {
+                        octopus.setCurrentCat(catCopy);
+                        viewDisplay.render();
+                    };
+                })(cat));
+
+                this.buttonArea.appendChild(entry);
+            }
         }
     };
 
@@ -59,43 +85,4 @@ $(function() {
 
 
     octopus.init();
-}());
-
-
-// // function addOne () {
-
-// //     clickCounter = clickCounter + 1;
-// //     countField.html(clickCounter);
-// //     console.log(clickCounter);
-// //     }
-
-// // $('#cat').click(addOne);
-
-// //new program:
-
-// let Cat = function(number, image) {
-//     this.name = 'Cat'+number;
-//     this.count = 0;
-//     this.image = image;
-// };
-
-// Cat.prototype.click = function() {
-//     this.count += 1;
-// };
-// Cat.prototype.build = function () {
-//     $('body').html(
-//         '<p>'+this.name+'</p>'
-//         //'<img src ="'+this.image+'">'
-//         //'<p>You have clicked '+this.count+' times</p>'
-//     );
-// };
-
-// const cat1 = new Cat(1, 'images/cat1.jpg');
-// const cat2 = new Cat(2, 'images/cat2.jpg');
-// let cats = [cat1, cat2];
-
-// document.onload(function (){
-//     cats.forEach(function (){
-//         this.build();
-//     });
-// });
+//}());
