a02b7d345d3b29cccd4c97af2dd84a5258117904
VolantTyler <33227593+VolantTyler@users.noreply.github.com>
2018-07-20 16:35:07 -0400

    fully functional

--- js/app.js
+++ js/app.js
@@ -2,48 +2,62 @@
 //$(function() {
 
     let model = {
-        currentCat: null;
+        currentCat: null,
         cats: [
         {
             name: 'Cat1',
             clickCount: 0,
+            //note first is .jpg, rest are .jpeg
             image: 'images/cat_picture1.jpg'
         },
         {
             name: 'Cat2',
             clickCount: 0,
-            image: 'images/cat_picture2.jpg'
+            image: 'images/cat_picture2.jpeg'
         },
         {
             name: 'Cat3',
             clickCount: 0,
-            image: 'images/cat_picture3.jpg'
+            image: 'images/cat_picture3.jpeg'
         },
         {
             name: 'Cat4',
             clickCount: 0,
-            image: 'images/cat_picture4.jpg'
+            image: 'images/cat_picture4.jpeg'
         },
         {
             name: 'Cat5',
             clickCount: 0,
-            image: 'images/cat_picture5.jpg'
+            image: 'images/cat_picture5.jpeg'
         }
-        ]};
+        ]
+    };
 
 
     let octopus = {
 
         init: function() {
+            model.currentCat = model.cats[0];
+
             viewButtons.init();
+            viewDisplay.init();
         },
 
         catsArray: function() {
-            return model;
+            return model.cats;
         },
 
         setCurrentCat: function(cat) {
             model.currentCat = cat;
+        },
+
+        getCurrentCat: function() {
+            return model.currentCat;
+        },
+
+        countPlusOne: function() {
+            model.currentCat.clickCount++;
+            viewDisplay.render();
         }
     };
 
@@ -60,7 +74,6 @@
             this.buttonArea.innerHTML = '';
             let cats = octopus.catsArray();
 
-            //remove model from view to octopus
             for(i=0; i<cats.length; i++) {
                 cat = cats[i];
                 entry = document.createElement('button');
@@ -80,7 +93,27 @@
 
 
     let viewDisplay = {
+        init: function() {
+            this.displayCat = document.getElementById('catNow');
+            this.displayName = document.getElementById('cat-name');
+            this.displayImage = document.getElementById('cat-image');
+            this.displayCount = document.getElementById('cat-count');
 
+            this.displayImage.addEventListener('click', function() {
+                octopus.countPlusOne();
+            });
+            
+            this.render();
+        },
+
+        render: function() {
+            let cat = octopus.getCurrentCat();
+
+            this.displayName.textContent = cat.name;
+            this.displayImage.src = cat.image;
+            this.displayCount.textContent = cat.clickCount;
+
+        }
     };
 
 
